d162791ca7325c194ffbe86b9190b542e4efca4e
Levine Hua <951995489@qq.com>
2021-11-30 11:30:20 +0800

    test

--- packages/finches-ui/version.ts
+++ packages/finches-ui/version.ts
@@ -1,9 +1,1 @@
-/*
- * @Description:
- * @Author: 华松林
- * @Date: 2021-11-29 16:07:26
- * @LastEditors: 华松林
- * @LastEditTime: 2021-11-30 10:01:58
- * @FilePath: /finches-ui/packages/finches-ui/version.ts
- */
-export const version = "0.0.0-dev.1";
+export const version = '0.0.0-dev.1'
